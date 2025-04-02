3ff390c357f36022538aa008d3680b73f1985d62
NikitaErmakov <ermafin@gmail.com>
2025-04-02 15:25:34 +0300

    more pythonic way

--- django_server/devices/tasks.py
+++ django_server/devices/tasks.py
@@ -20,17 +20,21 @@ def check_light_schedules():
     all_schedules = LightSchedule.objects.all()
     logger.info(f"Всего записей в LightSchedule: {all_schedules.count()}")
 
-    for schedule in all_schedules:
-        if schedule.should_trigger_now(now):
-            logger.info(f"Расписание ID {schedule.id} подходит. Включаем светильник.")
+    from .views import lamp
+    if not lamp:
+        logger.warning("Светильник не найден, невозможно включить свет.")
+        return
 
-            from .views import lamp
-            if lamp:
-                lamp.on(mode=1)
-                Light.objects.update_or_create(defaults={'is_on': True})
-                logger.info("Светильник включен.")
-            else:
-                logger.warning("Светильник не найден, невозможно включить свет.")
+    for schedule in all_schedules:
+        if not schedule.should_trigger_now(now):
+            continue
+
+        logger.info(f"Расписание ID {schedule.id} подходит. Включаем светильник.")
+
+        lamp.on(mode=1)
+        Light.objects.update_or_create(defaults={'is_on': True})
+        logger.info("Светильник включен.")
 
     logger.info("Задача check_light_schedules завершена.")
+
 
